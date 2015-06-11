0edbc60fd0195daefbef379af44c35d15983bb83
Murali Vilayannur <muraliv@pernixdata.com>
2015-06-11 16:21:49 -0700

    Trivial: Remove some useless declaration cruft

--- ntirpc/rpc/rpcent.h
+++ ntirpc/rpc/rpcent.h
@@ -67,7 +67,7 @@ extern struct rpcent *getrpcbynumber(int);
 extern struct rpcent *getrpcent(void);
 #endif
 
-extern void setrpcent(int) __THROW;
-extern void endrpcent(void) __THROW;
+extern void setrpcent(int);
+extern void endrpcent(void);
 __END_DECLS
 #endif				/* !_RPC_CENT_H */
